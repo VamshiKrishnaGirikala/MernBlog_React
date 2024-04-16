b9c6555db242bdb9c28020472709564c1f6156b9
VamshiGirikala <vamshi.girikala@svclnk.com>
2024-04-15 20:04:10 -0400

    Complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSideBar from '../components/DashSideBar';
+import DashProfile from '../components/DashProfile';
 const Dashboard = () => {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [location.search]);
     return (
-        <>
-            Dashboard
-        </>
+        <div className='min-h-screen flex flex-col md:flex-row'>
+            <div className='md:56'>
+                <DashSideBar />
+            </div>
+            <div className=''>
+                {tab === "profile" && <DashProfile />}
+            </div>
+        </div>
     )
 }
 
